90e6fc4e069059149eb502082f8df8774bb2a51b
falehomar <fibrahimomar@gmail.com>
2024-09-13 10:50:54 -0400

    sensor

--- src/app/sensor/sensor.component.ts
+++ src/app/sensor/sensor.component.ts
@@ -1,9 +1,12 @@
 import {Component, OnInit} from '@angular/core';
 import {DecimalPipe} from "@angular/common";
+import * as THREE from "three";
+import {GLTF, GLTFLoader} from "three/addons/loaders/GLTFLoader.js";
 
 interface SensorErrorEvent extends Event {
   error: DOMException;
 }
+
 enum SensorPermission {
   accelerometer = 'accelerometer',
   gyroscope = 'gyroscope',
@@ -16,9 +19,12 @@ interface SensorOptions {
 }
 
 interface Sensor {
-  start():void
-  stop():void
+  start(): void
+
+  stop(): void
+
   permissionPolicyName(): string
+
   permissions: SensorPermission[]
   activated: boolean
   hasReading: boolean
@@ -30,7 +36,7 @@ interface AbsoluteOrientationSensor extends EventTarget, Sensor, OrientationSens
   new(options?: SensorOptions): AbsoluteOrientationSensor;
 }
 
-declare var AbsoluteOrientationSensor : {
+declare var AbsoluteOrientationSensor: {
   prototype: AbsoluteOrientationSensor
   new(options?: SensorOptions): AbsoluteOrientationSensor
 }
@@ -39,83 +45,89 @@ interface RelativeOrientationSensor extends EventTarget, Sensor, OrientationSens
   new(options?: SensorOptions): RelativeOrientationSensor;
 }
 
-declare var RelativeOrientationSensor : {
+declare var RelativeOrientationSensor: {
   prototype: RelativeOrientationSensor
   new(options?: SensorOptions): RelativeOrientationSensor
 }
 
-interface OrientationSensor extends EventTarget, Sensor{
+interface OrientationSensor extends EventTarget, Sensor {
   new(options?: SensorOptions): OrientationSensor;
+
   quaternion: DOMPointReadOnly;
 }
 
-declare var OrientationSensor : {
+declare var OrientationSensor: {
   prototype: OrientationSensor
   new(options?: SensorOptions): OrientationSensor
 }
 
 
-interface Gyroscope extends EventTarget, Sensor{
+interface Gyroscope extends EventTarget, Sensor {
   new(options?: SensorOptions): Gyroscope;
+
   x: number;
   y: number;
   z: number;
 }
 
-declare var Gyroscope : {
+declare var Gyroscope: {
   prototype: Gyroscope
   new(options?: SensorOptions): Gyroscope
 }
 
-interface GravitySensor extends EventTarget, Sensor{
+interface GravitySensor extends EventTarget, Sensor {
   new(options?: SensorOptions): GravitySensor;
+
   illuminance: number;
 }
 
-declare var GravitySensor : {
+declare var GravitySensor: {
   prototype: GravitySensor
   new(options?: SensorOptions): GravitySensor
 }
 
-interface AmbientLightSensor extends EventTarget, Sensor{
+interface AmbientLightSensor extends EventTarget, Sensor {
   new(options?: SensorOptions): AmbientLightSensor;
+
   illuminance: number;
 }
 
-declare var AmbientLightSensor : {
+declare var AmbientLightSensor: {
   prototype: AmbientLightSensor
   new(options?: SensorOptions): AmbientLightSensor
 }
 
-interface Magnetometer extends EventTarget, Sensor{
+interface Magnetometer extends EventTarget, Sensor {
   new(options?: SensorOptions): Magnetometer;
+
   x: number;
   y: number;
   z: number;
 }
 
-declare var Magnetometer : {
+declare var Magnetometer: {
   prototype: Magnetometer
   new(options?: SensorOptions): Magnetometer
 }
 
-interface Accelerometer extends EventTarget, Sensor{
+interface Accelerometer extends EventTarget, Sensor {
   new(options?: SensorOptions): Accelerometer;
+
   x: number;
   y: number;
   z: number;
 }
 
-declare var Accelerometer : {
+declare var Accelerometer: {
   prototype: Accelerometer
   new(options?: SensorOptions): Accelerometer
 }
 
-interface LinearAccelerationSensor extends Accelerometer{
+interface LinearAccelerationSensor extends Accelerometer {
   new(options?: SensorOptions): LinearAccelerationSensor;
 }
 
-declare var LinearAccelerationSensor : {
+declare var LinearAccelerationSensor: {
   prototype: LinearAccelerationSensor
   new(options?: SensorOptions): LinearAccelerationSensor
 }
@@ -130,15 +142,19 @@ declare var LinearAccelerationSensor : {
   templateUrl: './sensor.component.html',
   styleUrl: './sensor.component.sass'
 })
-export class SensorComponent implements OnInit{
+export class SensorComponent implements OnInit {
+
+  absoluteOrientationSensor?: AbsoluteOrientationSensor
+
+  rotation?: DOMPointReadOnly
 
   accelerometer?: Accelerometer;
 
   acceleration: number = -1;
 
-  velocity: [number,number,number] = [0,0,0];
+  velocity: [number, number, number] = [0, 0, 0];
 
-  t0: [number,number,number] = [0,0,0]
+  t0: [number, number, number] = [0, 0, 0]
 
   timestamp: DOMHighResTimeStamp = new Date().getTime();
   hasReading: boolean = false;
@@ -146,33 +162,74 @@ export class SensorComponent implements OnInit{
 
   ngOnInit(): void {
 
-    // @ts-ignore
-    navigator.permissions.query({ name: "accelerometer" }).then((result) => {
-      if (result.state === "denied") {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    camera.position.z = 5;
+
+    const loader = new GLTFLoader();
+
+    loader.load('scene.gltf', function (gltf: GLTF) {
+      scene.add(gltf.scene);
+
+    }, undefined, function (error: unknown) {
+
+      console.error(error);
+
+    });
+
+
+    renderer.setAnimationLoop(() => {
+      if (this.rotation) {
+        scene.applyQuaternion(new THREE.Quaternion(this.rotation.x, this.rotation.y, this.rotation.z).invert());
+
+      }
+      renderer.render(scene, camera);
+    });
+
+    Promise.all([
+      // @ts-ignore
+      navigator.permissions.query({name: "accelerometer"}),
+      // @ts-ignore
+      navigator.permissions.query({name: "magnetometer"}),
+      // @ts-ignore
+      navigator.permissions.query({name: "gyroscope"}),
+    ]).then((results) => {
+      if (!results.every((result) => result.state === "granted")) {
         console.log("Permission to use accelerometer sensor is denied.");
         return;
       }
+      this.absoluteOrientationSensor = new AbsoluteOrientationSensor({frequency: 60})
+      this.absoluteOrientationSensor.addEventListener("reading", () => {
+        this.rotation = this.absoluteOrientationSensor?.quaternion
+      })
+
+      this.absoluteOrientationSensor.start()
       // Use the sensor.
-      this.accelerometer = new Accelerometer({frequency:60})
-      this.accelerometer.addEventListener("reading",() => {
+      this.accelerometer = new Accelerometer({frequency: 60})
+      this.accelerometer.addEventListener("reading", () => {
         if (this.accelerometer) {
-          this.acceleration = Math.hypot(this.accelerometer?.x, this.accelerometer?.y,this.accelerometer?.z);
+          this.acceleration = Math.hypot(this.accelerometer?.x, this.accelerometer?.y, this.accelerometer?.z);
 
           this.timestamp = this.accelerometer.timestamp
           this.hasReading = this.accelerometer.hasReading
           this.activated = this.accelerometer.activated
           // v = u + at
-          this.velocity = [this.t0[0]+this.accelerometer?.x/60,
-            this.t0[1]+this.accelerometer?.y/60,
-            this.t0[2]+this.accelerometer?.z/60,
+          this.velocity = [this.t0[0] + this.accelerometer?.x / 60,
+            this.t0[1] + this.accelerometer?.y / 60,
+            this.t0[2] + this.accelerometer?.z / 60,
           ]
 
-          this.t0 = [this.accelerometer?.x, this.accelerometer?.y,this.accelerometer?.z]
+          this.t0 = [this.accelerometer?.x, this.accelerometer?.y, this.accelerometer?.z]
 
         }
 
       })
       this.accelerometer.start()
     });
+
   }
 }
